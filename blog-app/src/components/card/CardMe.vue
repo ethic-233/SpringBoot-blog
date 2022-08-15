a4b9f884b45efb7495203120694e3285401bc24d
ethic-233 <3514171197@qq.com>
2022-08-15 19:08:46 +0800

    ++

---
+++ blog-app/src/components/card/CardMe.vue
@@ -0,0 +1,71 @@
+<template>
+  <el-card>
+    <h1 class="me-author-name">佳佳的博客</h1>
+    <div class="me-author-description">
+      <span><i class="el-icon-location-outline"></i> &nbsp;河北石家庄</span>
+      <span><i class="me-icon-job"></i> &nbsp;++</span>
+    </div>
+    <div class="me-author-tool">
+      <i @click="showTool(qq)" :title="qq.title" class="iconfont icon-qq"></i>
+      <i @click="showTool(bilibili)" :title="bilibili.title" class="iconfont icon-bilibili-fill"></i>
+      <i @click="showTool(github)" :title="github.title" class="iconfont icon-github"></i>
+    </div>
+  </el-card>
+
+</template>
+
+<script>
+  export default {
+    name: 'CardMe',
+    data() {
+      return {
+        qq: {title: 'QQ', message: '3514171197'},
+        bilibili: {
+          title: '哔哩哔哩',
+          message: '<a target="_blank" href="https://space.bilibili.com/43653228">https://space.bilibili.com/43653228</a>'
+        },
+        github: {
+          title: 'GitHub',
+          message: '<a target="_blank" href="https://github.com/ethic-233">https://github.com/ethic-233</a>'
+        }
+      }
+    },
+    methods: {
+      showTool(tool) {
+        this.$message({
+          duration: 0,
+          showClose: true,
+          dangerouslyUseHTMLString: true,
+          message: '<strong>' + tool.message + '</strong>'
+        });
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .me-author-name {
+    text-align: center;
+    font-size: 30px;
+    border-bottom: 1px solid #5FB878;
+  }
+
+  .me-author-description {
+    padding: 8px 0;
+  }
+
+  .me-icon-job {
+    padding-left: 16px;
+  }
+
+  .me-author-tool {
+    text-align: center;
+    padding-top: 10px;
+  }
+
+  .me-author-tool i {
+    cursor: pointer;
+    padding: 4px 10px;
+    font-size: 30px;
+  }
+</style>
